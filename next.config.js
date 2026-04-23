12fa59a13515d808608c2a2c9c1616d59d5d2817
vestibles <slychonzi@yahoo.com>
2026-04-23 22:09:56 +0100

    pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-const withPWA = require("next-pwa")({
+const nextPWA = require("next-pwa");
+
+const isDev = process.env.NODE_ENV === "development";
+
+const withPWA = nextPWA({
   dest: "public",
-  register: true,
-  skipWaiting: true,
+  disable: isDev,
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
+  turbopack: {}, // 👈 prevents that warning/error
 });
